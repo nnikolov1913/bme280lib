947388972d21272c18edcc4d5cc7feb31908257c
Nikolay Nikolov <dobrev666@gmail.com>
2024-02-25 22:29:32 +0100

    Sample app more readale

--- main.cpp
+++ main.cpp
@@ -17,18 +17,21 @@ int main(int argc, char *argv[])
 {
     unsigned count = 0;
     double temp, temp1;
+
     NickSensors *sensor = new NickSensors(IDrukSensor::BME280_INT_I2C);
     if (sensor && sensor->openSensor() == false) {
         std::cout << "BME280 sensor open ERROR " << std::endl;
         delete sensor;
         sensor = nullptr;
     }
+
     NickSensors *sensor1 = new NickSensors(IDrukSensor::FAKE_SENSOR);
     if (sensor1 && sensor1->openSensor() == false) {
         std::cout << "Fake sensor open ERROR " << std::endl;
         delete sensor1;
         sensor1 = nullptr;
     }
+
     while(count++ < 5) {
         if (sensor != nullptr) {
             if (sensor->getTemperature(temp)) {
@@ -59,11 +62,15 @@ int main(int argc, char *argv[])
         sensor->setThreshold(21.0, IDrukSensor::ABOVE_VALUE, [](void *data, double t)
                 { std::cout << "Temperature alarm " << t << "ᵒC" << std::endl;}, NULL);
     }
+
     if (sensor1 != nullptr) {
         sensor1->setThreshold(20.0, IDrukSensor::BELOW_VALUE, [](void *data, double t)
                 { std::cout << "Fake temperature alarm " << t << "ᵒC" << std::endl;}, NULL);
     }
+
+    //Wait for some alarms. At least from fake sensor should come
     std::this_thread::sleep_for(std::chrono::seconds(60));
+
     delete sensor;
     delete sensor1;
 }
